f13b2d7038509fc61d176abd247767a972935cea
chaos2171053 <qinliangchao@timework.cn>
2021-03-16 14:33:38 +0800

    feat:no order list

--- src/utils/hotKey.ts
+++ src/utils/hotKey.ts
@@ -1,1 +1,36 @@
-export default function hotKey(e: React.KeyboardEvent<HTMLTextAreaElement>) {}
+import insertTextAtCursor from "insert-text-at-cursor";
+const getKeycode = function (
+  e: React.KeyboardEvent<HTMLTextAreaElement> | any
+) {
+  let code;
+  if (e.key !== undefined) {
+    code = e.key;
+  } else if (e.keyIdentifier !== undefined) {
+    code = e.keyIdentifier;
+  } else if (e.keyCode !== undefined) {
+    code = e.keyCode;
+  }
+
+  return code;
+};
+
+function stopPropagation(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+export default function hotKey(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+  const keycode = getKeycode(e);
+  const target = e.target as HTMLTextAreaElement;
+  const starVal = target.value.substr(0, target.selectionStart);
+  const valArr = starVal.split("\n");
+  const currentLineStr = valArr[valArr.length - 1];
+  // enter
+  if (keycode === "Enter" || keycode === 13) {
+    // insert “no order list”
+    if (/^-\s/.test(currentLineStr)) {
+      stopPropagation(e);
+      insertTextAtCursor(target, `\n- `);
+    }
+  }
+}
