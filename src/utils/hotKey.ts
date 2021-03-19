e71af55999cb710c90a49671ffc783286cc4fc5d
chaos2171053 <chaos2171053@gmail.com>
2021-03-19 18:04:02 +0800

    ref:enter hotkey

--- src/utils/hotKey.ts
+++ src/utils/hotKey.ts
@@ -26,32 +26,44 @@ export default function hotKey(
 ) {
   const keycode = getKeycode(e);
   const target = e.target as HTMLTextAreaElement;
-  const starVal = target.value.substr(0, target.selectionStart);
+  const selectionStart = target.selectionStart;
+  const starVal = target.value.substr(0, selectionStart);
   const valArr = starVal.split("\n");
   const currentLineStr = valArr[valArr.length - 1];
   const orderListRegex = /^[1-9]\d*\.\d*|0\.\d*[1-9]\d*$/;
 
   // // enter
-  // if (keycode === "Enter" || keycode === 13) {
-  //   // insert “no order list”
-  //   if (currentLineStr.startsWith("- ")) {
-  //     stopPropagation(e);
-  //     // Cannot invoke an object which is possibly 'undefined'
-  //     (textareaIncetance as any).setTextareaValue(`${starVal}\n- `);
-  //     return;
-  //   } else if (orderListRegex.test(currentLineStr)) {
-  //     // inset order list
-  //     // currentLineStr could be null???
-  //     const orderNumber = parseInt(
-  //       (currentLineStr as any).match(orderListRegex)[0].split(".")[0],
-  //       10
-  //     );
-  //     if (orderNumber !== undefined) {
-  //       stopPropagation(e);
-  //       (textareaIncetance as any).setTextareaValue(
-  //         `${starVal}\n${orderNumber + 1}. `
-  //       );
-  //     }
-  //   }
-  // }
+  if (keycode === "Enter" || keycode === 13) {
+    // insert “no order list”
+    if (currentLineStr.startsWith("- ")) {
+      stopPropagation(e);
+      // Cannot invoke an object which is possibly 'undefined'
+      (textareaIncetance as any).insertText(`\n- `);
+      setTimeout(() => {
+        (textareaIncetance as any).setTextSelection({
+          start: selectionStart + 3,
+          end: selectionStart + 3,
+        });
+      });
+      return;
+    } else if (orderListRegex.test(currentLineStr)) {
+      // inset order list
+      // currentLineStr could be null???
+      const orderNumber = parseInt(
+        (currentLineStr as any).match(orderListRegex)[0].split(".")[0],
+        10
+      );
+      if (orderNumber !== undefined) {
+        stopPropagation(e);
+        const prefix = `\n${orderNumber + 1}. `;
+        (textareaIncetance as any).insertText(prefix);
+        setTimeout(() => {
+          (textareaIncetance as any).setTextSelection({
+            start: selectionStart + prefix.length,
+            end: selectionStart + prefix.length,
+          });
+        });
+      }
+    }
+  }
 }
